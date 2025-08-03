017e047f3b6670b23f8e403b576e3b07bc119358
eldani84 <danieleberhardt1984@gmail.com>
2025-08-02 23:22:54 -0300

    Fix rutas SPA Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // importante para Vercel o GitHub Pages
+  base: '/', // importante para Vercel o GitHub Pages
 })
